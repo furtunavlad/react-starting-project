10121cb7d99e174fd3fbb777bed85b14f2740bb2
Vlad Furtuna <144479033+furtunavlad@users.noreply.github.com>
2023-11-25 15:58:04 +0200

    final version

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./routes/Posts";
+import Posts, { loader as postsLoader } from "./routes/Posts";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import NewPost from "./routes/NewPost";
+import NewPost, { action as newPostAction } from "./routes/NewPost";
 import RootLayout from "./routes/RootLayout";
+import PostDetails, {loader as postDetailsLoader} from "./routes/PostDetails";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
     children: [
-      { path: "/", element: <App /> },
-      { path: "/new-post", element: <NewPost /> },
+      {
+        path: "/",
+        element: <Posts />,
+        loader: postsLoader,
+        children: [
+          { path: "/new-post", element: <NewPost />, action: newPostAction },
+          { path: "/:postId", element: <PostDetails />, loader: postDetailsLoader },
+        ],
+      },
     ],
   },
 ]);
--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,44 +1,47 @@
+import { Link, Form, redirect } from "react-router-dom";
+import Modal from "../components/Modal";
 import classes from "./NewPost.module.css";
-import { useState } from "react";
 
-function NewPost(props) {
-  const [enteredBody, setEnteredBody] = useState("");
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-
-  const [enteredAuthor, setEnteredAuthor] = useState("");
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
-
-  function submitHandler(event) {
-    event.preventDefault();
-    const postData = {
-      body: enteredBody,
-      author: enteredAuthor,
-    };
-    props.onAddPost(postData);
-    props.onCancel();
-  }
+function NewPost() {
   return (
-    <form className={classes.form} onSubmit={submitHandler}>
-      <p>
-        <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required onChange={authorChangeHandler} />
-      </p>
-      <p>
-        <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={bodyChangeHandler} />
-      </p>
-      <p className={classes.actions}>
-        <button type="button" onClick={props.onCancel}>
-          Cancel
-        </button>
-        <button>Submit</button>
-      </p>
-    </form>
+    <Modal>
+      <Form method="post" className={classes.form}>
+        <p>
+          <label htmlFor="name">Your name</label>
+          <input
+            type="text"
+            id="name"
+            name="author"
+            required
+          />
+        </p>
+        <p>
+          <label htmlFor="body">Text</label>
+          <textarea id="body" name="body" required rows={3}/>
+        </p>
+        <p className={classes.actions}>
+          <Link to=".." type="button">
+            Cancel
+          </Link>
+          <button>Submit</button>
+        </p>
+      </Form>
+    </Modal>
   );
 }
 
 export default NewPost;
+
+export async function action({request}) {
+  const formData = await request.formData();
+  const postData = Object.fromEntries(formData);
+  await fetch("http://localhost:8080/posts", {
+    method: "POST",
+    body: JSON.stringify(postData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  return redirect('/');
+}
